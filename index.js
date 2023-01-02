ffdc2bf94aebdfaa179d0d955035c4b402462d24
amanpadgelwar <113186236+amanpadgelwar@users.noreply.github.com>
2023-01-02 05:21:29 +0100

    added js variables to check values

---
+++ index.js
@@ -0,0 +1,9 @@
+var luckyNumber =document.querySelector(".LuckyNumber");
+var dob=document.querySelector(".DOB");
+var checkButton=document.querySelector(".check");
+
+checkButton.addEventListener("click"),function callback(){
+
+    console.log(luckyNumber.value)
+    console.log(dob.value);
+}
