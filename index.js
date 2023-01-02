18171aad14b2c82c0044be7349beb2de90a2316b
amanpadgelwar <113186236+amanpadgelwar@users.noreply.github.com>
2023-01-02 06:34:45 +0100

    added js

--- index.js
+++ index.js
@@ -2,8 +2,24 @@ var luckyNumber =document.querySelector(".LuckyNumber");
 var dob=document.querySelector(".DOB");
 var checkButton=document.querySelector(".check");
 
-checkButton.addEventListener("click"),function callback(){
 
-    console.log(luckyNumber.value)
-    console.log(dob.value);
+function luck()
+
+{
+
+ console.log(sumOfDigits(newDate));
 }
+
+function sumOfDigits(newDate){
+ 
+    var newDate =dob.value.replaceAll("-","");//converting date string to date string without hyphen
+
+        sum =0;
+        for (let i=0;i<newDate.length;i++){
+            sum=sum +number(newDate.charAt(i));
+        }
+        return sum;
+}
+
+
+checkButton.addEventListener('click',luck)
