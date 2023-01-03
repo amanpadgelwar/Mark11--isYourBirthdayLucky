a39f3905d6a0f2cbe5532d3341439c0eda138772
amanpadgelwar <113186236+amanpadgelwar@users.noreply.github.com>
2023-01-03 22:06:21 +0100

    whole program written

--- index.js
+++ index.js
@@ -1,25 +1,38 @@
-var luckyNumber =document.querySelector(".LuckyNumber");
-var dob=document.querySelector(".DOB");
+var inputNumber =document.querySelector(".LuckyNumber");
+
 var checkButton=document.querySelector(".check");
+var dob=document.querySelector(".DOB");
 
 
-function luck()
+  
 
-{
-
- console.log(sumOfDigits(newDate));
-}
 
 function sumOfDigits(newDate){
- 
     var newDate =dob.value.replaceAll("-","");//converting date string to date string without hyphen
-
-        sum =0;
-        for (let i=0;i<newDate.length;i++){
-            sum=sum +number(newDate.charAt(i));
+    sum=0; 
+   
+        for (let i=0;i<8;i++){
+            numberPos= Number(newDate.charAt(i));
+            sum=sum +numberPos
         }
         return sum;
 }
 
+
+
+function luck ()
+{
+   console.log(inputNumber.value);
+   console.log(sumOfDigits())
+console.log(sumOfDigits() %inputNumber.value)
+if (sumOfDigits() %inputNumber ===0){
+
+    console.log("Hurray!You are lucky")
+}
+else{
+    console.log("You are not so lucky afterall!")
+}
+
+}
 
 checkButton.addEventListener('click',luck)
